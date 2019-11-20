5f8dc2c8fe99cecbfef9cdf1405fefe63e75c8a3
techehub <vijeesh.tp@techehub.com>
2019-11-20 11:41:27 +0530

    working with Template

--- productinfo/urls.py
+++ productinfo/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from .views import displayProduct
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path ('user/', include('userapp.urls')),
-    path ('product/', include('productinfo.urls'))
+    path('product/<str:id>', displayProduct),
+
 
 ]
--- productinfo/views.py
+++ productinfo/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.template import loader
+
+from django.http import HttpResponse
+
+def displayProduct(request,id):
+
+
+
+    template=loader.get_template("product.html")
+
+    data ={
+
+        "1": {
+             "name" : "IPhone",
+            "desc" : "This is iPhone 6 smart phone",
+            "price" : 55000
+        },
+
+        "2": {
+            "name": "OPPO",
+            "desc": "This is OPPO A5 smart phone",
+            "price": 23000
+        }
+    }
+
+
+
+    res = template.render(data[id], request)
+    return HttpResponse(res)
